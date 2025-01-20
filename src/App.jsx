0e22871ac1a57fd66aaa6c3522e750c9bdbfd605
irina211093 <irinadanilkor@gmail.com>
2025-01-20 11:36:43 +0100

    add StartProject

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='post'>
+      <h1 className='post-title'>App</h1>
+      <p className='posz-desc'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+    </div>
+  )
+}
+
+export default App
+
